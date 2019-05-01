6451f5c870385536182dd776eaa17d4996ce8232
Reds <sahid.rojas@gmail.com>
2019-04-30 23:34:40 -0600

    Config tests

--- main.cpp
+++ main.cpp
@@ -16,8 +16,8 @@ int main(int argc, char *argv[])
     int b[9]={11,12,13,14,15,16,17,18,19};
     int c[9]={11,12,13,14,15,16,17,18,19};
     //char ruta[] = "hola sahid guapo ";
-    string rutaA="11-22-33-44-55-66-77-88-99";
-    string rutaB="12-42-43-64-75-86-77-08-59";
+    string rutaA="00-11-22-33-44-55-66-77-88-99";
+    string rutaB="00-01-02-03-04-05-06-07-08-09";
     string obstaculos="132-251-473";
     string json1=test1->SerializarInformacion(obstaculos,b,c,rutaA,rutaB,false,50);
     int z[10][10];
@@ -30,8 +30,8 @@ int main(int argc, char *argv[])
     string obstaculosJSON;
     test1->DeserializarInfoDeSimulacion(json1,&obstaculosJSON,x,y,&finalizacion,&AvanceGenetico,&rutaJSON,&rutaJSON2);
     cout<<rutaJSON2<<"hola loco"<<endl;
-//    Socket  *canal= &Socket::getInstance();
-//    canal->enviar(json1,8081,"192.168.100.15");
+    Socket  *canal= &Socket::getInstance();
+    canal->enviar(json1,8081,"192.168.100.9");
     //string respuesta=canal->escuchar(8080);
    return 0;
 }
--- traductorservidor.cpp
+++ traductorservidor.cpp
@@ -5,6 +5,46 @@ traductorServidor::traductorServidor()
 {
 
 }
+
+string traductorServidor::SerializarInformacionIteracion3(string obstaculos1, string rutaA, string rutaB, bool finalizacion)
+{
+    const char* json = "{\"AvanceGenetico\":0,"
+                       "\"finalizacion\":true,"
+                       "\"rutaA\":\"abcdefghaaa\","
+                       "\"rutaB\":\"abcdefghaaa\","
+                       "\"obstaculos\":\"abcdefghaaa\","
+                       "\"atributosGladiador1\":[0,2,0,0,0,0,0,0,0],"
+                       "\"atributosGladiador2\":[0,2,0,0,0,0,0,0,0],"
+                       "\"ganador\":\"\"}";
+    Document d;
+    d.Parse(json);
+    d["finalizacion"].SetBool(finalizacion);
+    if (!finalizacion) {
+        string stmp;
+        const char * cstr = rutaA.c_str();
+        const char * cstr1 = rutaB.c_str();
+        const char * obstaculos = obstaculos1.c_str();
+        d["obstaculos"].SetString((StringRef(obstaculos)));
+        d["rutaA"].SetString((StringRef(cstr)));
+        d["rutaB"].SetString((StringRef(cstr1)));
+    }
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    d.Accept(writer);
+    return buffer.GetString();
+}
+
+void traductorServidor::DeserializarInformacionIteracion3(string json,string *obstaculos1, string *rutaA, string *rutaB, bool *finalizacion)
+{
+    Document d;
+    d.Parse(json.c_str());
+     const char * cstr=d["rutaA"].GetString();
+     const char * obstac=d["obstaculos"].GetString();
+    *rutaA=d["rutaA"].GetString();
+    *rutaB=d["rutaB"].GetString();
+    *obstaculos1=d["obstaculos"].GetString();
+    *finalizacion=d["finalizacion"].GetBool();
+}
 void traductorServidor::DeserializarInfoDeSimulacion(string json,string *obstaculos,int atributosGladiador1[9],int atributosGladiador2[9],bool *finalizacion,int *AvanceGenetico,string *rutaA,string *rutaB)
 {
     Document d;
