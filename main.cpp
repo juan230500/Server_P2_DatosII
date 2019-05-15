5466169ac5aab2016aa5c268be159abf2de2268d
Reds <sahid.rojas64@gmail.com>
2019-05-15 00:30:25 -0600

    Se agregan los puntos extra

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ int main(){
 
     Server *server = new Server();
     Socket* socket = &Socket::getInstance();
-    string ip = "192.168.100.17";
+    string ip = "192.168.100.20";
     while (true) {
         server->Play(ip);
         delete server;
